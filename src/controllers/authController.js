dc2bf92f06a58d4f44592240a4e8519e5857695b
KevinT78 <kevindune@hotmail.fr>
2025-02-28 12:11:36 +0100

    Creation et login pour l'admin

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,25 +1,60 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+const Admin = require('../models/Admin');
 
-// Exemple de données utilisateur (à remplacer par une base de données)
-const adminUser = {
-  username: 'admin',
-  password: bcrypt.hashSync('admin_password', 10), // Mot de passe haché
-  role: 'admin'
-};
+const SECRET_KEY = process.env.SECRET_KEY ;
 
-const SECRET_KEY = process.env.SECRET_KEY || 'votre_clé_secrète';
-
-exports.login = (req, res) => {
+// Connexion Admin
+exports.login = async (req, res) => {
   const { username, password } = req.body;
 
-  if (username === adminUser.username && bcrypt.compareSync(password, adminUser.password)) {
-    const token = jwt.sign({ userId: adminUser.username, role: adminUser.role }, SECRET_KEY, {
-      expiresIn: '1h'
+  try {
+    const admin = await Admin.findOne({ username });
+    if (!admin) {
+      return res.status(401).json({ message: 'Admin non trouvé.' });
+    }
+
+    const isMatch = await bcrypt.compare(password, admin.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: 'Mot de passe incorrect.' });
+    }
+
+    const token = jwt.sign({ adminId: admin._id, role: admin.role }, SECRET_KEY, {
+      expiresIn: '24h'
     });
 
     return res.json({ token });
+  } catch (err) {
+    return res.status(500).json({ message: 'Erreur serveur' });
   }
+};
 
-  return res.status(401).json({ message: 'Nom d\'utilisateur ou mot de passe incorrect.' });
+// Création d'un admin (à faire une seule fois)
+exports.register = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const existingAdmin = await Admin.findOne({ username });
+    if (existingAdmin) {
+      return res.status(400).json({ message: 'Cet admin existe déjà.' });
+    }
+
+    const newAdmin = new Admin({ username, password });
+    await newAdmin.save();
+
+    res.status(201).json({ message: 'Admin créé avec succès.' });
+  } catch (err) {
+    res.status(500).json({ message: 'Erreur serveur' });
+  }
+};
+
+
+
+exports.checkAdminExists = async (req, res) => {
+  try {
+    const adminCount = await Admin.countDocuments();
+    res.json({ exists: adminCount > 0 });
+  } catch (err) {
+    res.status(500).json({ message: 'Erreur serveur' });
+  }
 };
